16308aa7a1ba7e02aa04d4f54e1b7040441bf110
Tyler Ritone <tylerritone@Tylers-MacBook-Air.local>
2023-02-25 12:09:36 -0500

    update server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const apiroutes = require("./routes/apiroutes");
 const htmlroutes = require("./routes/htmlroutes");
 
 const app = express();
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
